0fbaa24c93cbf621a5bb6f6c318af3fc1afe6d0e
ntello <team@unarix.com.ar>
2020-09-23 22:57:15 -0300

    eze

--- eze.c
+++ eze.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void numColor(short color, short numero);
+void numColor(short color, short numero); // prototipo
 
 int main()
 {
     short i=1;
 
-    while(i<101)
+    while(i<101) // llamo varias veces a la funcion
     {
-        numColor(i,i);
-        i = i + 1;
+        numColor(i,i); // llamada a la funcion
+        i = i + 1; // incremento
     }
     
-    printf("\n");
+    printf("\n"); //lo agrego solo par acrear un salto de linea
 
     return 0;
 }
 
-void numColor(short color, short numero)
+void numColor(short color, short numero) // firma
 {
-    printf("\033[0;%hdm", color);
+    printf("\033[0;%hdm", color); // cuerpo
     printf("%hd ", numero);
     printf("\033[0m");
 }
