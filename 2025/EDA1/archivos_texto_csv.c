6861fdd151b691c0181ca96adb838000b85a8961
nhtello <team@unarix.com.ar>
2025-05-27 17:42:50 -0300

    Update archivos_texto_csv.c

--- 2025/EDA1/archivos_texto_csv.c
+++ 2025/EDA1/archivos_texto_csv.c
@@ -5,21 +5,34 @@
 
 int main() {
     FILE *archivo;
-    char buffer[MAX_LINE]; // No explicar al principio.
+    // No explicar al principio.
+    char buffer[MAX_LINE];
 
-    // 1. Escribir en el archivo CSV
+    // Ahora lo que necesitamos es abrir el archivo.
+    // Cual era el modo que si no existia lo crea? "WT"
+    // como hacia esto?
     archivo = fopen("agenda.csv", "wt");
+
+    // Siempre que abro un archivo necesito comprobar el acceso, como lo hago?
     if (archivo == NULL) {
         perror("Error al abrir el archivo para escribir");
         return 1;
     }
 
+    // Entonces yo voy a intentar escribir en mi archivo CSV datos que estan separados por comas
+    // Que funcion me permite poner texto en mi archivo? FPUTS()
     // Escribir encabezado y algunos datos
     fputs("Nombre,Telefono,Email\n", archivo);
     fputs("Juan Perez,1234567890,juan@example.com\n", archivo);
     fputs("Ana Gomez,9876543210,ana@example.com\n", archivo);
     fputs("Carlos Ruiz,5551234567,carlos@example.com\n", archivo);
 
+    // Correr el programa!
+    // Mirar el archivo!
+    // Esta todo ok?
+
+    // Entonces ahora vamos a leerlo.
+    // que tengo que hacer si se que voy a necesitar en el mismo programa volver a abrir el archivo?
     fclose(archivo);
 
     // 2. Leer el archivo CSV
@@ -30,14 +43,26 @@ int main() {
     }
 
     printf("Contenido del archivo agenda.csv:\n\n");
-	
+
 	fgets(buffer, MAX_LINE, archivo);
     while (!feof(archivo)) {
         printf("%s", buffer);  // fgets ya incluye el salto de línea
+		fgets(buffer, MAX_LINE, archivo);
+    }
+
+    // Supongamos que llegue quiero volver a leer? como puedo hacer para rebovinar?
+    rewind(archivo);  // Volver al principio del archivo
+
+    printf("Leyendo de nuevo desde el principio:\n\n");
+
 	fgets(buffer, MAX_LINE, archivo);
+    while (!feof(archivo)) {
+        printf("%s", buffer);  // fgets ya incluye el salto de línea
+		fgets(buffer, MAX_LINE, archivo);
     }
 
     fclose(archivo);
 
     return 0;
+
 }
