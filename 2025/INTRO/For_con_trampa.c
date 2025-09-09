6dd7f536d1aa20ed72f8374be068105f0e87d8c8
nhtello <team@unarix.com.ar>
2025-09-09 20:18:00 -0300

    Create For_con_trampa.c

---
+++ 2025/INTRO/For_con_trampa.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+//Hacé un programa que pida al usuario ingresar dos números enteros: un valor inicial y un valor final. 
+//Luego, usá un bucle for para imprimir todos los números pares entre esos dos valores.
+
+// Los alumnos suelen asumir que siempre se empieza de menor a mayor, por lo tanto escriben algo como:
+// for (int i = inicio; i <= fin; i++) { ... }
+
+int main() {
+    int inicio, fin;
+
+    printf("Ingrese el valor inicial: ");
+    scanf("%d", &inicio);
+
+    printf("Ingrese el valor final: ");
+    scanf("%d", &fin);
+
+    if (inicio <= fin) {
+        for (int i = inicio; i <= fin; i++) {
+            if (i % 2 == 0) {
+                printf("%d ", i);
+            }
+        }
+    } else {
+        // Si los valores están al revés, el bucle debe ir hacia atrás
+        for (int i = inicio; i >= fin; i--) {
+            if (i % 2 == 0) {
+                printf("%d ", i);
+            }
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
