079680570ec9fe5f97196a3f6a567f3281f2fd69
nhtello <team@unarix.com.ar>
2025-10-07 20:46:45 -0300

    Update String_strCompare.c

--- 2025/INTRO/String_strCompare.c
+++ 2025/INTRO/String_strCompare.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <string.h> // Necesario para usar strcmp()
 
-/* ingrese dos cadenas de caracteres y comparelas */
+/* ingrese dos cadenas de caracteres y comparelas 
+
+¿Qué hace strcmp()?
+    strcmp(cadena1, cadena2) compara carácter por carácter.
+    esta dentro de la libreria <string.h>
+Como trabaja?
+    Compara dos cadenas carácter por carácter, utilizando el código ASCII de cada letra.
+    Paso a paso explicado:
+        Compara el primer carácter de cada string.
+        Si son iguales, pasa al siguiente.
+        Repite esto hasta encontrar una diferencia o llegar al final ('\0').
+    Si encuentra una diferencia:
+        Resta los códigos ASCII de los caracteres actuales.
+        Devuelve ese resultado:
+            0 → las cadenas son iguales
+            >0 → el primer carácter distinto en s1 es mayor que el de s2
+            <0 → el carácter en s1 es menor que el de s2
+    
+*/
 
 int main() {
     char cadena1[100];
