8541163193b38325fae14e9c86eed02430a15cf2
nhtello <team@unarix.com.ar>
2025-10-07 20:12:24 -0300

    Create Array_struct_sueldos.c

---
+++ 2025/INTRO/Array_struct_sueldos.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#define CANT 2
+
+// Realice un programa que me permita cargar los datos de los empleados.
+// Datos: Nombre, DNI, Sueldo, Horas_Extra
+// En base a su sueldo y las horas extras registradas, calcular su sueldo total.
+// Si el empleado supera los 10.000 pesos de sueldo debe pagar impuesto a las ganancias.
+
+struct persona {
+    char nombre[20];
+    long DNI;
+    float sueldo;
+    short h_extras;
+};
+
+float calcularSueldo(struct persona emp);
+
+int main(void)
+{
+    struct persona empleados[CANT];;
+    short i=0;
+    short cont = 0;
+
+    do
+    {
+       // Ingresamos los datos del empleado
+        printf("Ingrese el nombre del empleado: \n");
+        scanf("%s", empleados[cont].nombre);
+        printf("Ingrese el DNI: \n");
+        scanf("%ld", &empleados[cont].DNI);
+        printf("Ingrese el sueldo bruto: \n");
+        scanf("%f", &empleados[cont].sueldo);
+        printf("Ingrese horas extras: \n");
+        scanf("%hd", &empleados[cont].h_extras);
+
+        empleados[cont].sueldo = calcularSueldo(empleados[cont]);
+        cont++;
+
+    } while(cont<CANT);
+
+
+    // Imprimimos los datos de los empleados
+    for (i=0;i<CANT;i++)
+    {
+        printf("\n ** Sueldo de este mes para %s: ** \n", empleados[i].nombre);
+        printf("  DNI: %ld\n",empleados[i].DNI);
+        printf("  Sueldo: %.3f \n",empleados[i].sueldo);
+    }
+
+    // Busqueda LINEAL!!!
+    printf("\n ** Los empleados que deben pagar impuesto a las ganancias son: ** \n");
+    for (i=0;i<CANT;i++)
+    {
+        if(empleados[i].sueldo>10000)
+        {
+            printf(" - %s, debe pagar ganancias! \n", empleados[i].nombre);
+        }
+    }
+
+    return 0;
+}
+
+// Agrgear el calculo de impuestos *0.17 o *17/100
+float calcularSueldo(struct persona emp)
+{
+    short hora_neta = (emp.sueldo / 21) / 8;
+    float sueldo = emp.sueldo + (hora_neta * emp.h_extras);
+    return sueldo;
+}
