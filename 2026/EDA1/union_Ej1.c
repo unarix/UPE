5e63ba4e0ccfdcf074f76031c047234353a63e83
nhtello <team@unarix.com.ar>
2026-03-17 19:42:39 -0300

    Enhance union example with detailed comments
    Updated comments and added explanation for union usage.

--- 2026/EDA1/union_Ej1.c
+++ 2026/EDA1/union_Ej1.c
@@ -1,8 +1,28 @@
+/*
+Union — UNA sola caja, varios usos
+	Imaginá esto:
+	Tenés una sola caja , pero la usás para guardar distintas cosas en distintos momentos:
+	a la mañana → guardás un sandwich
+	a la tarde → guardás herramientas
+	a la noche → guardás ropa
+	La caja es la misma, no crece, no cambia de tamaño
+	Solo cambia lo que hay adentro
+Lo importante:
+	Todos los campos comparten la misma memoria
+	Entonces:
+	cuando escribís en uno… pisás el anterior
+	Resultado: los valores anteriores se pierden
+Tamaño de la union
+	Es igual al campo más grande
+	
+*/
+	
 #include <stdio.h>
 
+// Cuanto puede almacenar esta union?
 union Dato {
-	int entero;
-	float decimal;
+	int entero; // 4 bytes
+	float decimal; // 1 byte
 };
 
 int main() {
