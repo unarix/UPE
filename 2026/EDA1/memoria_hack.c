4500f4b9d656ad1d8c9212a028e3818ff8ecc2c4
nhtello <team@unarix.com.ar>
2026-04-28 21:44:47 -0300

    Add memoria_hack.c with buffer overflow example
    Implement a vulnerable program that demonstrates buffer overflow.

---
+++ 2026/EDA1/memoria_hack.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	char buffer[8];
+	int autenticado = 0;
+		
+	if (argc < 2) {
+		printf("Uso: %s <password>\n", argv[0]);
+		return 1;
+	}
+	
+	strcpy(buffer, argv[1]); // Copia el valor del argumento enviado en buffer. El problema? desborda el buffer.
+	
+	if (strcmp(buffer, "admin") == 0) { // con el buffer desbordado aca no entra, pero autenticado es cualquier cosa que no sea 0
+		autenticado = 1;
+	}
+	
+	if (autenticado) { // como autenticado tiene basura del desborde no es 0, si no es 0 entonces es verdadero.
+		printf("Acceso permitido\n");
+	} else {
+		printf("Acceso denegado\n");
+	}
+	
+	return 0;
+}
