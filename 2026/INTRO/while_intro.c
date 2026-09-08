8ce01aba4c4527f1be3a5d38107ab5214ce2cc49
nhtello <team@unarix.com.ar>
2026-09-08 17:48:34 -0300

    Update while_intro.c

--- 2026/INTRO/while_intro.c
+++ 2026/INTRO/while_intro.c
@@ -5,28 +5,31 @@
 // Ejemplo, Suma diez veces el numero 1, de uno en uno y guarde su resultado en una variable total.
 
 int main() {
-    short sumador = 1; // 1. Inicialización de la variable de control
-
-    // Bueno, da la forma que podriamos hacerlo usando lo que sabemos es: 
-    /*
-    sumador = sumador + 1;
-    sumador = sumador + 1;
-    sumador = sumador + 1;
-    sumador = sumador + 1;
-    sumador = sumador + 1;
-    sumador = sumador + 1;
-    sumador = sumador + 1;
-    sumador = sumador + 1;
-    sumador = sumador + 1;
-    */
-
-    // Una de las cosas para lo que me sirve el while es para ahorrarme codigo, repetir el codigo que esta dentro tantas veces como la condicion lo diga.
-    // Condición de permanencia, mientras la condicion sea verdadera, ejecuta el codigo que esta dentro.
-    while (sumador <= 10) {
-        sumador = sumador + 1; // 3. Actualización (paso hacia el fin del bucle)
-    }
-
-    printf("Sumador = %hd \n", sumador);
-    printf("¡Bucle terminado!\n");
-    return 0;
+	short sumador = 0; // 1. Inicialización de la variable de control
+	
+	// Bueno, da la forma que podriamos hacerlo usando lo que sabemos es: 
+	/*
+	sumador = sumador + 1;
+	sumador = sumador + 1;
+	sumador = sumador + 1;
+	sumador = sumador + 1;
+	sumador = sumador + 1;
+	sumador = sumador + 1;
+	sumador = sumador + 1;
+	sumador = sumador + 1;
+	sumador = sumador + 1;
+	*/
+	
+	// Una de las cosas para lo que me sirve el while es para ahorrarme codigo, repetir el codigo que esta dentro tantas veces como la condicion lo diga.
+	// Condición de permanencia, mientras la condicion sea verdadera, ejecuta el codigo que esta dentro.
+	while (sumador <= 10) {
+		sumador = sumador + 1; // 3. Actualización (paso hacia el fin del bucle)
+	}
+	
+	// Explicar el concepto de bucle infinito, no incrementando nunca el sumador.
+	// la variable nunca cambia, la condición siempre es verdadera y el programa queda colgado consumiendo recursos.
+	
+	printf("Sumador = %hd \n", sumador);
+	printf("¡Bucle terminado!\n");
+	return 0;
 }
