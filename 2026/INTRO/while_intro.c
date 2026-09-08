d1d6fbd06b011e761f0ed92ac48458be2ac4b0e6
nhtello <team@unarix.com.ar>
2026-09-08 17:42:21 -0300

    Refactor while_intro.c for clarity and functionality
    Updated comments and added repetitive increment statements for clarity.

--- 2026/INTRO/while_intro.c
+++ 2026/INTRO/while_intro.c
@@ -2,14 +2,23 @@
 
 // El while es como un if pero un if evalúa una condición una sola vez, en cambio el while la evalúa de forma repetitiva hasta que la condicion no sea verdadera.
 
-// Ejemplo, Suma diez veces el numero 1 y guarde su resultado en una variable total.
+// Ejemplo, Suma diez veces el numero 1, de uno en uno y guarde su resultado en una variable total.
 
 int main() {
     int sumador = 1; // 1. Inicialización de la variable de control
 
+    sumador = sumador + 1;
+    sumador = sumador + 1;
+    sumador = sumador + 1;
+    sumador = sumador + 1;
+    sumador = sumador + 1;
+    sumador = sumador + 1;
+    sumador = sumador + 1;
+    sumador = sumador + 1;
+    sumador = sumador + 1;
+    
     // 2. Condición de permanencia
     while (contador <= 10) {
-        printf("Sumando 1 a sumador: %d\n", sumador);
         sumador = sumador + 1; // 3. Actualización (paso hacia el fin del bucle)
     }
 
