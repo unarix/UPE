bd1ab3a15fcd8b2fc7bec51360e5057b292c5d77
nhtello <team@unarix.com.ar>
2026-09-15 19:13:47 -0300

    Create struct_principiante.c

---
+++ 2026/INTRO/struct_principiante.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* 1. Definicion del struct */
+struct Estudiante {
+    char curso;
+    short edad;
+    float promedio;
+    short legajo;
+};
+
+int main(void) {
+    /* 2. Declaracion e inicializacion (ANSI C exige declarar variables al inicio del bloque) */
+    struct Estudiante alumno1 = {"A", 21, 9.5, 4439};
+    alumno1.legajo = 5050;
+  
+    /* 3. Acceso a los campos con el operador punto (.) */
+    printf("--- Datos del Estudiante ---\n");
+    printf("Nombre: %s\n", alumno1.nombre);
+    printf("Edad: %d anos\n", alumno1.edad);
+    printf("Promedio: %.1f\n", alumno1.promedio);
+    printf("Promedio: %.1f\n", alumno1.promedio);
+
+    return 0;
+}
