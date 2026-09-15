0cef72c531b94b7ca8f1faa0977972bbcac63904
nhtello <team@unarix.com.ar>
2026-09-15 20:19:34 -0300

    Change struct fields to short and add fan usage

--- 2026/INTRO/strcut_cpu.c
+++ 2026/INTRO/strcut_cpu.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
 struct Procesador {
-    int id_core;
-    int temp_celsius;
-    int mhz;
+	short id_core;
+	short temp_celsius;
+	short mhz;
+	short uso_ventilador;
 };
 
 int main(void) {
-    /* Declaracion ANSI C */
-    struct Procesador cpu = {0, 85, 4200};
-
-    printf("Core #%d: %d°C a %d MHz\n", cpu.id_core, cpu.temp_celsius, cpu.mhz);
-
-    /* Proteccion termica por hardware */
-    if (cpu.temp_celsius > 80) {
-        cpu.mhz = 1500;
-        printf("¡Alerta de calor! Frecuencia reducida a %d MHz\n", cpu.mhz);
-    }
-
-    return 0;
+	/* Declaracion ANSI C con tipos short */
+	struct Procesador cpu = {0, 85, 4200, 50};
+	
+	printf("Core #%hd: %hd Grados C a %hd MHz | Ventilador: %hd%%\n", 
+		   cpu.id_core, cpu.temp_celsius, cpu.mhz, cpu.uso_ventilador);
+	
+	/* Proteccion termica por hardware */
+	if (cpu.temp_celsius > 80) {
+		cpu.mhz = 1500;
+		cpu.uso_ventilador = 100;
+		
+		printf("¡Alerta de calor! Frecuencia reducida a %hd MHz | Ventilador: %hd%%\n", 
+			   cpu.mhz, cpu.uso_ventilador);
+	}
+	
+	return 0;
 }
